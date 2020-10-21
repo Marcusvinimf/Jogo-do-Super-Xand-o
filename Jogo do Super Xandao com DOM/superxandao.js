f8b1e2312245b734d27b60c5c8a34a92a5e177fb
Marcusvinimf <marcus_v_ina@hotmail.com>
2020-10-21 14:41:32 -0300

    adicionando ultimas alterações do meu jogo com o DOM

--- Jogo do Super Xandao com DOM/superxandao.js
+++ Jogo do Super Xandao com DOM/superxandao.js
@@ -1,35 +1,25 @@
 
 function inicio(){
 
+    var resultado = document.querySelector('div#resultado')
+
     titulo.innerHTML = "SUPER XANDÃO O ULTIMO HEROI DA TERRA";
 
     paragrafo.innerHTML = "Super Xandão acorda de seu castelo, São exatamente 3:33 da manhã.<br><br><br>Super Xandão saiu para caçar demônios!!!";
 
-    botao.innerHTML = "Comece sua aventura!!!";
-
-    botao.onclick = primeiraFase;
+    resultado.innerHTML = '<button onclick = "primeiraFase()">Comece sua aventura!!!</button>';
 
 }
 
 function primeiraFase(){
+    
+    var resultado = document.querySelector('div#resultado')
 
     titulo.innerHTML = "Primaria Fase";
 
     paragrafo.innerHTML = "Super Xandão avistou um poderoso demônio lvl 3";
 
-
-    var resultado = document.querySelector('div#resultado')
-
-    resultado.innerHTML = '<button id="b1" onclick = "derrota()">1</button id="b2"><br><br><br><button onclick = "ataquePrimeiraFase()">2</button>'
-
-
-
-    // botao.innerText = "Não lutar";
-    
-    // // botao2.innerHTML = '<button>"ENCARAR ESTILO SUPER XANDÃO!!!"</button>'
-    
-    // botao.onclick = derrota;
-    // // botao2.onclick = ataquePrimeiraFase;
+    resultado.innerHTML = '<button onclick = "derrota()">Não lutar</button><br><br><br><button onclick = "ataquePrimeiraFase()">ENCARAR ESTILO SUPER XANDÃO</button>';
 
 }
 
@@ -39,29 +29,53 @@ function ataquePrimeiraFase(){
 
     paragrafo.innerHTML = "Super Xandão carrega um poderoso ataque ao demônio lvl 3";
 
-    resultado.innerHTML = 'ajhfgvwgf'
+    resultado.innerHTML = '<button onclick = "ataque()">SEM PRESSÃO, AQUI É XANDÃO!!!</button>';
 
+}
 
-//     button1.innerText = "SEM PRESSÃO, AQUI É XANDÃO!!!";
+function ataque(){
+            
+    var ataqueX = Math.round(Math.random () * 8);
 
-//     var ataqueX = Math.round(Math.random () * 8);
+    if (ataqueX == 7 || ataqueX == 8 )
+    {
+        document.write("Super Xandão estraçalha o demônio!!! todos os outros demônios ficaram com medo, MUNDO SALVO PRA SEMPRE!!! <br><br><br> FECHE O JOGO!!!");
 
-//     function ataque(){
+    } else if (ataqueX == 2 || ataqueX == 3 || ataqueX == 4 || ataqueX == 5 || ataqueX == 6)
+        {
+        onclick = segundaFase("                   VITORIA!!!\n \n \nSuper Xandão lutou bem, como sempre, continue salvando o mundo Super Xandão!!!");
+        }else{
+        onclick = derrota("Super Xandão teve piedade desse demônio e o deixou ir");
+    }
+}
 
-//         if (ataqueX == 6 || ataqueX == 7 || ataqueX == 8 )
-//         {
-//             document.write("Super Xandão estraçalha o demônio!!! todos os outros demônios ficaram com medo, MUNDO SALVO PRA SEMPRE!!! <br><br><br> FECHE O JOGO!!!");
+function segundaFase(){
 
-//         } else if (ataqueX == 3 || ataqueX == 4 || ataqueX == 5 )
-//             {
-//             alert("                   VITORIA!!!\n \n \nSuper Xandão lutou bem, como sempre, continue salvando o mundo Super Xandão!!!");
-//             }else{
-//             alert("Super Xandão teve piedade desse demônio e o deixou ir");
-//         }
-//     }
+    titulo.innerHTML = "ULTIMO DEMONIO CONTRA NOSSO HEROI";
 
-//     button1.onclick = ataque;
+    paragrafo.innerHTML = "Super Xandão vence sua ultima batalha de forma justa, porém ninguém tem chance contra nosso heroi, mas um ultimo inimigo surge, oque vc vai fazer Super Xandão???";
 
+    resultado.innerHTML = '<button onclick = "derrota()">Não lutar dessa vez</button><br><br><br><button onclick = "ataqueSegundaFase()">ENCARAR ESTILO SUPER XANDÃO <strong> SEMPRE!!!<strong></button>';
+
+}
+
+function ataqueSegundaFase(){
+
+    titulo.innerHTML = "ATAQUE DE SUPER XANDÃO";
+
+    paragrafo.innerHTML = "Super Xandão carrega um poderoso ataque contra seu ultimo inimigo";
+
+    resultado.innerHTML = '<button onclick = "ataqueDeVitoria()">SUPER DOUBLE BICEPS!!!</button><br><br><br><button onclick = "derrota2()">Ataque Terra Redonda</button>';
+
+}
+
+function ataqueDeVitoria(){
+
+    titulo.innerHTML = "MUNDO SALVO PRA SEMPRE";
+
+    paragrafo.innerHTML = "Super Xandão tem alma de campeão, nunca perde uma luta, mundo permanecerá salvo enquanto Super Xandão existir";
+
+    resultado.innerHTML = '<button onclick = "inicio()">CONTINUE SALVANDO O MUNDO SUPER XÃNDÃO!!!</button>'
 }
 
 function derrota(){
@@ -70,13 +84,16 @@ function derrota(){
 
     paragrafo.innerHTML = "O MUNDO FOI TOMADO!!!";
 
-    // botao.innerHTML = "SALVE O MUNDO SUPER XÃNDÃO!!!";
+    resultado.innerHTML = '<button onclick = "inicio()">SALVE O MUNDO SUPER XÃNDÃO!!!</button>'
 
-    var b1 = document.getElementById('b1')
+}
 
-    resultado.innerHTML = 'SCARLET SALVOU'
+function derrota2(){
 
-    b1.innerHTML = 'wfuwfo'
-    
-    // botao.onclick = inicio;
+    titulo.innerHTML = "DERROTA";
+
+    paragrafo.innerHTML = "A TERRA FICOU PLANA QUANDO SUPER XANDÃO PISOU NELA!!!";
+
+    resultado.innerHTML = '<button onclick = "inicio()">SALVE O MUNDO SUPER XÃNDÃO!!!</button>'
+
 }
